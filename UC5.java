5323ab81eb0c60ba4adc4b7526a08624cf8ace6b
Avinash8899 <sg1021@srmist.edu.in>
2026-02-24 13:26:22 +0530

    uc5 changes

--- UC5.java
+++ UC5.java
@@ -1,5 +1,17 @@
-public class UC5 {
-    public static void main (String[] args){
+public class UC5{
+    public static void main(String[] args){
+        String [] line = {
+        (String.join(" " ,"   ***   ","   ***   ","******   ","  ***** ")),
+        (String.join(" "," **   ** "," **   ** ","**    ** ","**       ")),
+        (String.join(" ","**     **","**     **","**    ** "," **      ")),
+        (String.join(" ","**     **","**     **","******   ","   ***   ")),
+        (String.join(" ","**     **","**     **","**       ","      ** ")),
+        (String.join(" "," **   ** "," **   ** ","**       ","**     **")),
+        (String.join(" ","   ***   ","   ***   ","**       ","  *****  ")),
+        };
         
+        for(int i=0;i<7;i++){
+            System.out.println(line[i]);
+        }
     }
 }
